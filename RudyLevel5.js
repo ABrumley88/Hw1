9787440e3e15edab4846c16248b2d89e37c823a4
ABrumley88 <brumley@cca.edu>
2017-01-22 16:20:28 -0800

    Create RudyLevel5.js

---
+++ RudyLevel5.js
@@ -0,0 +1,38 @@
+right();
+right();
+while (getColor() == "red") {
+  up();
+}
+while (getColor() == "blue") {
+  down();
+}
+right();
+right();
+while (getColor() == "blue") {
+  down();
+}
+right();
+right();
+while (getColor() == "blue") {
+  down();
+}
+while (getColor() == "red") {
+  up();
+}
+right();
+right();
+right();
+while (getColor() == "blue") {
+  down();
+}
+while (getColor() == "red") {
+  up();
+}
+right();
+right();
+right();
+while (getColor() == "blue") {
+  down();
+}
+right();
+right();
