5419f63aa18bb41365e6ea0ad0146da6779ca6d3
ABrumley88 <brumley@cca.edu>
2017-01-22 16:22:02 -0800

    Create RudyLevel8.js

---
+++ RudyLevel8.js
@@ -0,0 +1,50 @@
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+repeatDown(8);
+right();
+right();
+function repeatUp(total) {
+  var count = 0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+repeatUp(8);
+right();
+right();
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+repeatDown(8);
+right();
+right();
+function repeatUp(total) {
+  var count = 0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+repeatUp(8);
+right();
+right();
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+repeatDown(8);
+right();
+right();
